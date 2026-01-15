dc3238ff4fb825e59512ae1084792eca5d02eccd
seunga418 <seungab77@gmail.com>
2026-01-15 09:17:02 +0900

    study react

--- section03/src/index.js
+++ section03/src/index.js
@@ -2,6 +2,9 @@ import mul, { add, sub } from "./math.js"; // 같은 경로라면 합쳐서 불�
 
 import randomColor from 'randomcolor';
 
+const color = randomColor();
+console.log(color);
+
 console.log(add(1, 2));
 console.log(sub(1, 2));
-console.log(mul(2, 3))
+console.log(mul(2, 3));
